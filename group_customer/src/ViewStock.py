6e9d784e86c7636dfe14aff6bf213f324abe8940
2466402m <2466402m@student.gla.ac.uk>
2024-04-04 08:33:25 +0100

    adding utils

--- group_customer/src/ViewStock.py
+++ group_customer/src/ViewStock.py
@@ -1,10 +1,11 @@
 import csv, os
-#from utils.TableBuilder import TableBuilder
+from utils.TableBuilder import TableBuilder
 #from src.PurchaseOptions import PurchaseOptions
+from input_validation import *
 
 class ViewStock:
 
-    #I've kept this in from CustomerLoad - happy to get rid of it if it's not needed
+    #I've kept this in from Login - happy to get rid of it if it's not needed
     def fix_working_directory(self):
         current_working_directory = os.getcwd()
         while "test" in current_working_directory or "src" in current_working_directory:
@@ -15,29 +16,38 @@ class ViewStock:
         self.fix_working_directory()
         stock = []
         with open("resource/" + file_name, 'r') as file:
-            file_reader = csv.reader(file)
+            file_reader = csv.DictReader(file)
             for row in file_reader:
                 stock.append(row)
         return stock
     
-    def display_stock(stock): #include utils.TableBuilder
+    def display_stock(self, stock):
         table_headers = ["Row Number", "Item Name", "Item Price", "Available Quantity"]
-        data = [row["row_number"], row["item_name"], row["item_price"], row["available_quantity"] for row in stock]
-        # table = TableBuilder.build_table(table_headers, data)
+        data = [[row["row_number"], row["item_name"], row["item_price"], row["available_quantity"]] for row in stock]
+        table = TableBuilder()
+        table.add_headers(table_headers)
+        table.add_rows(data)
+        table.build()
 
     
     def select_item(stock):
-        row_num = int(input("Please enter the row number of the item you want to select: "))
-        # implement input validation here?
+        while True:
+            row_num = input("Please enter the row number of the item you want to select: ")
+            validate_type(row_num, "int")
+            row_num = int(row_num)
+            validate_range(row_num, 1, len(stock))
+            return stock[row_num-1] #not sure if this implements the input validation properly?
+
     
 
     def main():
         stock = read_stock_data("shopstock.csv")
-        display_stock(stock)
-        selected_item = select_item(stock)
+        self.display_stock(stock)
+        selected_item = self.select_item(stock)
         PurchaseOptions.main(selected_item)
     
     if __name__ == "__main__":
-        main()
+        view_stock = ViewStock()
+        view_stock.main()
 
     
