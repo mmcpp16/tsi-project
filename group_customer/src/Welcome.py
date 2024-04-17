f911cb6f35488345ce82180165ec71dcc5931af9
AndyMckelvey <150909606+AndyMckelvey@users.noreply.github.com>
2024-04-17 15:02:44 +0100

    Refactored welcome

--- group_customer/src/Welcome.py
+++ group_customer/src/Welcome.py
@@ -1,17 +1,16 @@
 from src.ViewStock import ViewStock
-class Welcome:
-    def welcome(self):
-        print("""
-                         _____       _           _            
-                        /__   \_   _| |__  _   _| | __ _ _ __ 
-                          / /\/ | | | '_ \| | | | |/ _` | '__|
-                         / /  | |_| | |_) | |_| | | (_| | |   
-                         \/    \__,_|_.__/ \__,_|_|\__,_|_|      
-                    """)
-        enter = input("Welcome to Tubular! Press enter to continue!".rjust(65))
-        if enter == "":
-            print("Here is the stock:")
-            ViewStock.main()
-        else:
-            print("That's not how you enter :(")
-            Welcome.welcome(self)
+def welcome():
+    print("""
+                        _____       _           _            
+                       /__   \_   _| |__  _   _| | __ _ _ __ 
+                         / /\/ | | | '_ \| | | | |/ _` | '__|
+                        / /  | |_| | |_) | |_| | | (_| | |   
+                        \/    \__,_|_.__/ \__,_|_|\__,_|_|      
+                """)
+    enter = input("Welcome to Tubular! Press enter to continue!".rjust(65))
+    if enter == "":
+        print("Here is the stock:")
+        ViewStock.main()
+    else:
+        print("That's not how you enter :(")
+        welcome()
