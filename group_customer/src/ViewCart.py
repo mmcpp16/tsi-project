b074b6536f4fcf3df38f3755c7c8b77df7a62066
Andrew Mckelvey <2843209M@student.gla.ac.uk>
2024-04-17 16:05:41 +0100

    added duration to table and formatted rent v buy

--- group_customer/src/ViewCart.py
+++ group_customer/src/ViewCart.py
@@ -3,9 +3,12 @@ from utils.TableBuilder import *
 
 def view_cart(cart):  # assuming cart is a list of items
     cart_table = TableBuilder(num_column=False)
-    headers = ["Item Name", "Price", "Quantity"]
+    headers = ["Item Name", "Price", "Quantity", "Duration"]
     cart_table.add_headers(headers)
-    cart_table.add_rows(cart)
+
+    formatted_cart = [(item[0], item[1], item[2], "Life" if item[3] == 0 else item[3]) for item in cart]
+
+    cart_table.add_rows(formatted_cart)
     cart_table.build()
 
     subtotal = 0
@@ -17,15 +20,15 @@ def view_cart(cart):  # assuming cart is a list of items
 
 if __name__ == '__main__':
     thecart = [
-        ["Beach Towel", 15.99, 100],
-        ["Sunscreen SPF 50", 8.99, 50],
-        ["Sunglasses", 25.50, 75],
-        ["Beach Umbrella", 29.99, 30],
-        ["Beach Chair", 24.99, 40],
-        ["Flip Flops", 12.50, 80],
-        ["Volley Ball", 19.99, 60],
-        ["Surf Board (Adult)", 121.99, 25],
-        ["Surf Board (Child)", 104.99, 20],
-        ["Inflatable Pool Float", 18.99, 50]
+        ["Beach Towel", 15.99, 100, 0],
+        ["Sunscreen SPF 50", 8.99, 50, 0],
+        ["Sunglasses", 25.50, 75, 7],
+        ["Beach Umbrella", 29.99, 30, 4],
+        ["Beach Chair", 24.99, 40, 7],
+        ["Flip Flops", 12.50, 80, 2],
+        ["Volley Ball", 19.99, 60, 1],
+        ["Surf Board (Adult)", 121.99, 25, 1],
+        ["Surf Board (Child)", 104.99, 20, 1],
+        ["Inflatable Pool Float", 18.99, 50, 7]
     ]
     view_cart(thecart)
