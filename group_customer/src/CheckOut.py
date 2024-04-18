f7bbb8cf407745a1d401703ab2e45af4c9b3e0c1
Andrew Mckelvey <2843209M@student.gla.ac.uk>
2024-04-18 11:47:48 +0100

    removed clutter from branch & added database functionality

--- group_customer/src/CheckOut.py
+++ group_customer/src/CheckOut.py
@@ -1,10 +1,11 @@
 from ViewCart import view_cart
 from Welcome import welcome
+from utils.DatabaseHelper import DatabaseHelper
 from utils.input_validation import get_valid_range
 
 
 def check_out(cart):
-    view_cart(cart)  # Assuming ViewCart uses table builder and takes in a list of 'item' objects
+    view_cart(cart)  # Assuming ViewCart is an array of tuples with # (ID) (Name) (Amount) (Price) (Duration)
 
     prompt = "Do you accept this? \n 1 = Yes \n 0 = No \n"
     choice = get_valid_range(prompt, 0, 1)
@@ -15,11 +16,17 @@ def check_out(cart):
         first_name = input("what is your first name? \t: ")
         last_name = input("what is your last name? \t: ")
         pass_phrase = input("what is your pass phrase \t: ")
-
         collect_time = "2024-04-26 12:00:00"  # temporary implementation
 
-        data_entry = (first_name, last_name, pass_phrase, collect_time)
-        #Not sure how to add to database from here, should that be an external function?
+        add_data = DatabaseHelper()
+        new_cart = []
+        # Takes in (Item ID, Quantity, Duration)
+        for item in cart:
+            new_cart.append(
+                (item[0], item[2], item[4])
+            )
+
+        add_data.add_customer_order(first_name, last_name, pass_phrase, collect_time, new_cart)
 
         print(f"Thanks {first_name}! Make sure to use the phrase \"{pass_phrase}\" at the pick up desk!")
     else:
@@ -28,18 +35,14 @@ def check_out(cart):
     input("Press enter to return home")
     welcome()
 
+
 # Example usecase
 # if __name__ == '__main__':
 #     the_cart = [
-#         ["Beach Towel", 15.99, 100, 0],
-#         ["Sunscreen SPF 50", 8.99, 50, 0],
-#         ["Sunglasses", 25.50, 75, 7],
-#         ["Beach Umbrella", 29.99, 30, 4],
-#         ["Beach Chair", 24.99, 40, 7],
-#         ["Flip Flops", 12.50, 80, 2],
-#         ["Volley Ball", 19.99, 60, 1],
-#         ["Surf Board (Adult)", 121.99, 25, 1],
-#         ["Surf Board (Child)", 104.99, 20, 1],
-#         ["Inflatable Pool Float", 18.99, 50, 7]
+#         [0, "Beach Towel", 100, 15.99, 0],
+#         [1, "Sunscreen SPF 50", 50, 8.99, 0],
+#         [2, "Sunglasses", 75, 25.50, 7],
+#         [3, "Beach Umbrella", 30, 29.99, 4],
+#         [4, "Beach Chair", 40, 24.99, 7],
 #     ]
 #     check_out(the_cart)
